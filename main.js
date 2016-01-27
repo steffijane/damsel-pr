9d1d317caa1264c0ec56dac10c68b3a872ec50ce
steffijane <balarezos@gmail.com>
2016-01-26 22:48:48 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+window.onload=function(){
+	var nav = document.getElementById('nav');
+	var colors = ['black', '#ef5a7c']
+
+	while(i <= 2){
+		nav.style.backgroundColor = colors[i];
+		i += 1;
+	}
+}
